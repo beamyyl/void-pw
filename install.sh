10c10dbaa7bc7791c1eb1dff32823a259a1b369b
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-11 09:59:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo xbps-remove -Ry pulseaudio
 sudo xbps-install -Sy pipewire wireplumber alsa-pipewire libspa-bluetooth pavucontrol
 
@@ -12,11 +13,14 @@ sudo ln -sf /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
 sudo ln -sf /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
 
 mkdir -p "$HOME/.config/autostart"
-cp /usr/share/applications/pipewire.desktop "$HOME/.config/autostart/"
-cp /usr/share/applications/pipewire-pulse.desktop "$HOME/.config/autostart/"
-cp /usr/share/applications/wireplumber.desktop "$HOME/.config/autostart/"
-echo "Done! If you're running a wm, make sure to include:"
-echo 'pipewire &' 
-echo 'wireplumber &'
-echo 'pipewire-pulse &`'
-echo "either in your .xinitrc, or in your wm's config file."
+ln -sf /usr/share/applications/pipewire.desktop "$HOME/.config/autostart/"
+ln -sf /usr/share/applications/pipewire-pulse.desktop "$HOME/.config/autostart/"
+
+echo "--------------------------------------------------------"
+echo "Done! If you're running a Window Manager (i3, sway....),"
+echo "make sure to include these lines in your startup config:"
+echo ""
+echo "pipewire &"
+echo "pipewire-pulse &"
+echo "wireplumber &"
+echo "--------------------------------------------------------"
