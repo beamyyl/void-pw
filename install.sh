f4870a33f891aa27e8dc23c03fb4a8ec54214c93
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-27 22:53:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,2 @@
-#!/bin/sh
-sudo xbps-remove -Ry pulseaudio
-sudo xbps-install -Sy pipewire wireplumber alsa-pipewire libspa-bluetooth pavucontrol turnstile
-sudo ln -sf /etc/sv/dbus /var/service/
-sudo ln -sf /etc/sv/turnstiled /var/service/
-
-sudo mkdir -p /etc/pipewire/pipewire.conf.d
-sudo ln -sf /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
-sudo ln -sf /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
-
-sudo mkdir -p /etc/alsa/conf.d
-sudo ln -sf /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
-sudo ln -sf /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
-
-mkdir -p "$HOME/.config/autostart"
-ln -sf /usr/share/applications/pipewire.desktop "$HOME/.config/autostart/"
-
-echo "---------------------------------------------------------"
-echo 'Done! If you are running a Window Manager (i3, sway....),'
-echo "make sure to include these lines in your startup config:"
-echo "pipewire &"
-echo "--------------------------------------------------------"
+sudo xbps-install -Sy pipewire wireplumber pipewire-alsa turnstile dbus && sudo ln -s /etc/sv/turnstiled /var/service/ && sudo ln -s /etc/sv/dbus /var/service/ && echo 'pipewire > /dev/null 2>&1 & wireplumber > /dev/null 2>&1 & pipewire-pulse > /dev/null 2>&1 &' >> .bash_profile && echo 'pipewire > /dev/null 2>&1 & wireplumber > /dev/null 2>&1 & pipewire-pulse > /dev/null 2>&1 &' >> .zprofile
+echo 'Done. You can now reboot or re-login'
