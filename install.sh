106c86648c087f0f23c15d9b3557e0fc877314c7
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-07-03 13:10:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,27 +15,27 @@ ln -sf /usr/share/examples/pipewire/20-pipewire-pulse.conf "$HOME/.config/pipewi
 echo ""
 echo "========================================="
 echo "Choose your PipeWire autostart method:"
-echo "1) Shell Profile (~/.bash_profile & ~/.zprofile)"
-echo "2) XDG Autostart (~/.config/autostart)"
+echo "1) XDG Autostart (~/.config/autostart)"
+echo "2) Shell Profile (~/.bash_profile & ~/.zprofile)"
 echo "========================================="
 printf "Enter your choice (1 or 2): "
 read choice
 
 case $choice in
     1)
-        echo "Option 1 selected (Shell Profile)"
+        echo "Option 1 selected (XDG Autostart)"
+        sleep 1
+        mkdir -p "$HOME/.config/autostart"
+        ln -sf /usr/share/applications/pipewire.desktop "$HOME/.config/autostart/"
+        echo "Linked pipewire.desktop to user autostart folder."
+        ;;
+    2)
+        echo "Option 2 selected (Shell Profile)"
         sleep 1
         CMD="if ! pgrep -x \"pipewire\" > /dev/null; then pipewire >/dev/null 2>&1 & fi"
         echo "$CMD" >> "$HOME/.bash_profile"
         echo "$CMD" >> "$HOME/.zprofile"
         echo "Added pw to shell profile."
-        ;;
-    2)
-        echo "Option 2 selected (XDG Autostart)"
-        sleep 1
-        mkdir -p "$HOME/.config/autostart"
-        ln -sf /usr/share/applications/pipewire.desktop "$HOME/.config/autostart/"
-        echo "Linked pipewire.desktop to user autostart folder."
         ;;
     *)
         echo "Invalid choice. Skipping autostart configuration."
